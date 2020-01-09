4e2bfe66f35ab2a266f196c90d035781caaa89ab
KoKoSiC007 <okint34@yandex.ru>
2020-01-10 00:21:06 +0300

    Фикс бага с двумя формами

--- web/js/lost_and_find_drons/scripts.js
+++ web/js/lost_and_find_drons/scripts.js
@@ -1,10 +1,22 @@
+const addStyleOnForm = (formID) => {
+    let is_close =  $('.img-one').hasClass('img-container--left') && $('.img-two').hasClass('img-container--right');
+    if (is_close){
+        document.getElementById('lost_form').classList.add('hidden');
+        document.getElementById('find_form').classList.add('hidden');
+    } else {
+        document.getElementById(`${formID}_form`).classList.toggle('hidden');
+    }
+};
+
 $('#find').on('click', () => {
-    document.getElementById('find_form').classList.toggle('hidden');
+    addStyleOnForm('find');
+
     document.querySelector('.img-one').classList.toggle('img-container--left');
     document.querySelector('.img-two').classList.toggle('img-container--right');
 });
 $('#lost').on('click', () => {
-    document.getElementById('lost_form').classList.toggle('hidden');
+    addStyleOnForm('lost');
+
     document.querySelector('.img-one').classList.toggle('img-container--left');
     document.querySelector('.img-two').classList.toggle('img-container--right');
 });
