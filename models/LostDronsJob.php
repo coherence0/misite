050019c7f36ab73576fd148d407d96a642ffb037
Mitsko <44983999+Mitsko@users.noreply.github.com>
2020-01-12 13:46:52 +0300

    fixerrors

--- models/LostDronsJob.php
+++ models/LostDronsJob.php
@@ -106,7 +106,7 @@ class LostDronsJob extends BaseObject implements \yii\queue\JobInterface
 		foreach($alfbarr as $aw){
 		            if($aw!=""){
 		                   $val="";
-		                   if (!($aw, $hschar))
+		                   if (!in_array($aw, $hschar))
 		                   	continue;
 		                   $val = $hschar[$aw];
 		                   if($val){ 
