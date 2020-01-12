7d92657d0eab985d24e386ec10a9c5d0029c6e4e
KoKoSiC007 <okint34@yandex.ru>
2020-01-12 15:28:03 +0300

    Рендер формы номера один раз

--- web/js/lost_and_find_drons/scripts.js
+++ web/js/lost_and_find_drons/scripts.js
@@ -1,14 +1,24 @@
-const addStyleOnForm = (formID) => {	
+
+const addStyleOnForm = (formID) => {
 	let is_close = $('.img-one').hasClass('img-container--left') && $('.img-two').hasClass('img-container--right');
 	
 	if (is_close){
 		document.getElementById('lost_form').classList.add('hidden');
 		document.getElementById('find_form').classList.add('hidden');
+		document.getElementById('findedDronePhoneForm').classList.add('hidden');
 	} else {
 		document.getElementById(`${formID}_form`).classList.toggle('hidden');
+		document.getElementById('findedDronePhoneForm').classList.toggle('hidden');
+		addStyleOnButton(formID)
 	}
 };
-
+const addStyleOnButton = (formID) => {
+	if (formID === 'find'){
+		$('#phoneConfirmBtn').removeClass('yellow__bg').addClass('red__bg');
+	} else {
+		$('#phoneConfirmBtn').removeClass('red__bg').addClass('yellow__bg');
+	}
+};
 $('#find').on('click', () => {
 	addStyleOnForm('find');
 
